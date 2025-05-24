42749aac2d5d3b1e22a6d1a45e2c603b27f0241c
TsukiYami <tsukiyamigaming@gmail.com>
2025-05-25 01:13:23 +0200

    Changes 25/05/2025
    Adding file AES, Hashing, RSA and rename to SecurityHandler + implement singleton

---
+++ ToDoList/Security/SecurityHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Security
+{
+	public class SecurityHandler
+	{
+		public Hashing oHashing;
+		private static SecurityHandler instance;
+
+		public SecurityHandler()
+		{
+			oHashing = new Hashing();
+		}
+		private static SecurityHandler Instance{
+			get{
+				if(instance == null){
+					instance = new SecurityHandler();
+				}
+				return instance;
+			}
+		}
+
+		public Hashing hashing()
+		{
+			return oHashing;
+		}
+	}
+}
