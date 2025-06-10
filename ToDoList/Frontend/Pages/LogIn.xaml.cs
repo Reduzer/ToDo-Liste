b1b628802fce623538d7c9821c50a3521abfa5c5
TsukiYami <tsukiyamigaming@gmail.com>
2025-06-10 18:24:15 +0200

    Changes 10/06/2025

---
+++ ToDoList/Frontend/Pages/LogIn.xaml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend.Pages;
+
+public partial class LogIn : ContentPage
+{
+    public LogIn()
+    {
+        InitializeComponent();
+    }
+
+    private void OnLoginButtonClicked(object? sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
